2e0f38674365ba88356eea5043e464f3fdff1147
nikitazapekin <zapekinnikitamain@gmail.com>
2025-10-23 02:32:43 +0300

    last

---
+++ WebApplication5/App_Start/RouteConfig.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication5
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            // Игнорировать маршруты для Web Forms страниц
+            routes.IgnoreRoute("Test.aspx");
+            routes.IgnoreRoute("Default.aspx");
+            routes.IgnoreRoute("Products.aspx");
+            routes.IgnoreRoute("Register.aspx");
+
+            // Игнорировать все .aspx файлы
+            routes.IgnoreRoute("{*allaspx}", new { allaspx = @".*\.aspx(/.*)?" });
+
+            // Web Forms маршруты ДО MVC маршрутов
+            routes.MapPageRoute(
+                "TestRoute",
+                "test",
+                "~/Test.aspx"
+            );
+
+            routes.MapPageRoute(
+                "DefaultRoute",
+                "",
+                "~/Default.aspx"
+            );
+
+            routes.MapPageRoute(
+                "ProductsRoute",
+                "products",
+                "~/Products.aspx"
+            );
+
+            routes.MapPageRoute(
+                "RegisterRoute",
+                "register",
+                "~/Register.aspx"
+            );
+
+            // MVC маршруты ПОСЛЕ Web Forms маршрутов
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
+/* using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication5
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapPageRoute(
+              "TestRoute",
+              "test",
+              "~/Test.aspx"
+          );
+
+
+        }
+    }
+}
+*/
---
+++ WebApplication5/Test.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Test.aspx.cs" Inherits="WebApplication5.Test" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            tessttstts
+        </div>
+    </form>
+</body>
+</html>
