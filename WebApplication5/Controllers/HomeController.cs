f8f0c514dcc6b1b7402ad8583afb2a93577697b4
Global Name <zapekinnikitamain@gmail.com>
2025-12-01 11:11:17 +0300

    lasy

--- WebApplication5/Controllers/HomeController.cs
+++ WebApplication5/Controllers/HomeController.cs
@@ -15,14 +15,13 @@ namespace WebApplication5.Controllers
 
         public ActionResult About()
         {
-         //   ViewBag.Message = "Your application description page.";
-
+       
             return View();
         }
 
         public ActionResult Contact()
         {
-          //  ViewBag.Message = "Your contact page.";
+       
 
             return View();
         }
